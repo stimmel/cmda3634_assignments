dd0df181cef8ccf2709de4047327479a23065ef2
stimmel <stimmel@princeton.edu>
2018-08-23 14:38:11 -0400

    Added comments to show where code should go

--- HW01/intersections.c
+++ HW01/intersections.c
@@ -5,16 +5,18 @@
 //if there is no intersection, this function should return 0
 //Otherwise, populate 'intersection' with the point of intersection and return 1
 int ray_disk_intersection(struct ray observer, struct disk obj, double *intersection) {
-
+  //Question 3: Revise this function so it correctly computes an intersection
   return 0;
 }
 
 //if there is no intersection, this function should return 0
 //Otherwise, populate 'intersection' with the point of intersection and return 1
 int ray_cylinder_intersection(struct ray observer, struct cylinder obj, double *intersection) {
+  //Question 5: Revise this function so it correctly computes an intersection
   return 0;
 }
 
 int ray_cone_intersection(struct ray observer, struct cone obj, double *intersection) {
+  //Question 7: Revise this function so it correctly computes an intersection
   return 0;
 }
