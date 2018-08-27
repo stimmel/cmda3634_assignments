785fa0f3ef83bfbb40d2e321ff123ee6938c5c0a
stimmel <stimmel@princeton.edu>
2018-08-27 01:24:50 -0400

    Added a makefile, changed object names, and added many more tests

--- HW01/intersections.h
+++ HW01/intersections.h
@@ -1,68 +1,68 @@
 //Define vectors component-wise
-typedef struct vector {
+typedef struct vector_t {
   double x;
   double y;
   double z;
-} vector;
+} vector_t;
 
 //Rays are determined by their start point and direction vector
-typedef struct ray {
-  vector start;
-  vector dir;
-} ray;
+typedef struct ray_t {
+  vector_t start;
+  vector_t dir;
+} ray_t;
   
 //A disk is determined by its radius, center, and normal vector
-typedef struct disk {
+typedef struct disk_t {
   double radius;
-  vector center;
-  vector normal;
-} disk;
+  vector_t center;
+  vector_t normal;
+} disk_t;
 
 //A open cylinder is determined by its radius, height, the center of its base,
 //and a normal vector to its base
-typedef struct cylinder {
+typedef struct cylinder_t {
   double radius;
   double height;
-  vector center;
-  vector axis;
-} cylinder;
+  vector_t center;
+  vector_t axis;
+} cylinder_t;
 
 //An open right cone is determined by radius, height, its vertex, and a vector from the vertex normal to the base
-typedef struct cone {
+typedef struct cone_t {
   double radius;
   double height;
-  vector vertex;
-  vector axis;
-} cone;
+  vector_t vertex;
+  vector_t axis;
+} cone_t;
 
 //a sphere is determined by its center and radius
-typedef struct sphere {
+typedef struct sphere_t {
   double radius;
-  vector center;
-} sphere;
+  vector_t center;
+} sphere_t;
 
 //utility functions: Basic vector operations
-double dot_product(vector a, vector b);
-vector cross_product(vector a, vector b);
-double distance(vector a, vector b);
-double magnitude(vector a);
-vector sum(vector a, vector b);
-vector difference(vector a, vector b);
-vector scaled_sum(double c1, vector a, double c2, vector b);
-vector scaled_difference(double c1, vector a, double c2, vector b);
-vector scalar_product(double c, vector a);
-void copy_vector(vector a, vector *result);
+double dot_product(vector_t a, vector_t b);
+vector_t cross_product(vector_t a, vector_t b);
+double distance(vector_t a, vector_t b);
+double magnitude(vector_t a);
+vector_t sum(vector_t a, vector_t b);
+vector_t difference(vector_t a, vector_t b);
+vector_t scaled_sum(double c1, vector_t a, double c2, vector_t b);
+vector_t scaled_difference(double c1, vector_t a, double c2, vector_t b);
+vector_t scalar_product(double c, vector_t a);
+void copy_vector(vector_t a, vector_t *result);
 
 //utility functions: object constructors
-vector new_vector(double x, double y, double z);
-ray new_ray(vector start, vector dir);
-disk new_disk(double radius, vector center, vector normal);
-cylinder new_cylinder(double radius, double height, vector center, vector axis);
-cone new_cone(double radius, double height, vector vertex, vector axis);
-sphere new_sphere(double radius, vector center);
+vector_t new_vector(double x, double y, double z);
+ray_t new_ray(vector_t start, vector_t dir);
+disk_t new_disk(double radius, vector_t center, vector_t normal);
+cylinder_t new_cylinder(double radius, double height, vector_t center, vector_t axis);
+cone_t new_cone(double radius, double height, vector_t vertex, vector_t axis);
+sphere_t new_sphere(double radius, vector_t center);
 
 //Object intersections
-int ray_sphere_intersection(ray observer, sphere obj, vector *intersection);
-int ray_disk_intersection(ray observer, disk obj, vector *intersection);
-int ray_cylinder_intersection(ray observer, cylinder obj, vector *intersection);
-int ray_cone_intersection(ray observer, cone object, vector *intersection);
+int ray_sphere_intersection(ray_t observer, sphere_t obj, vector_t *intersection);
+int ray_disk_intersection(ray_t observer, disk_t obj, vector_t *intersection);
+int ray_cylinder_intersection(ray_t observer, cylinder_t obj, vector_t *intersection);
+int ray_cone_intersection(ray_t observer, cone_t object, vector_t *intersection);
--- HW01/main.c
+++ HW01/main.c
@@ -5,97 +5,200 @@
 int main(int argc, char ** argv) {
 
   //temporary variables used in testing
-  vector center;
-  vector vertex;
-  vector normal;
-  vector axis;
+  vector_t center;
+  vector_t vertex;
+  vector_t normal;
+  vector_t axis;
   
-  //an observer peering in a random direction
-  vector obs_start = new_vector(0.1,0.5,1.);
-  vector obs_dir = new_vector(0.8,-0.6,0.);
-  ray observer = new_ray(obs_start,obs_dir);
+  //an observer peering in a predictable direction
+  vector_t obs_start = new_vector(0.1,0.5,1.);
+  vector_t obs_dir = new_vector(0.8,-0.6,0.);
+  ray_t observer = new_ray(obs_start,obs_dir);
 
-  //a sphere in his way
+  //second observer, more random direction
+  obs_start = new_vector(0.5,-0.2,-0.8);
+  obs_dir = new_vector(0.707106781,0.577350269,0.40824829);
+  ray_t observer2 = new_ray(obs_start,obs_dir);
+
+  //third observer, perfect diagonal
+  obs_start = new_vector(-2.,3.,5.);
+  obs_dir = new_vector(0.577350269,0.577350269,0.577350269);
+  ray_t observer3 = new_ray(obs_start,obs_dir);
+  
+  //positive test, observer 1
   center = new_vector(2.5,-1.3,1.);
-  sphere yes_sphere = new_sphere(1.,center);
+  sphere_t yes_sphere = new_sphere(1.,center);
 
-  vector sphere_intersection_pt = new_vector(1.7,-0.7,1.);
+  vector_t sphere_intersection_pt = new_vector(1.7,-0.7,1.);
 
-  //a sphere out of his way
+  //negative test, observer 1
   center = new_vector(3.,-1.7,2.);
-  sphere no_sphere_1 = new_sphere(0.5,center);
+  sphere_t no_sphere_1 = new_sphere(0.5,center);
 
-  //a sphere behind him
+  //edge case: a sphere behind observer 1
   center = new_vector(-0.7,1.1,1.);
-  sphere no_sphere_2 = new_sphere(0.5,center);
+  sphere_t no_sphere_2 = new_sphere(0.5,center);
   
-  //a disk in his way
+  //positive test, observer 1
   center = new_vector(0.8,0.,1.2);
   normal = new_vector(0.96,-0.28,0.);
-  disk yes_disk = new_disk(2.5,center,normal);
+  disk_t yes_disk_1 = new_disk(2.5,center,normal);
 
-  vector disk_intersection_pt = new_vector(0.794017094,-0.020512821,1.);
+  vector_t disk_intersection_pt1 = new_vector(0.794017094,-0.020512821,1.);
+
+  //positive test, observer 2
+  center = new_vector(1.1,0.3,-0.4);
+  normal = new_vector(0.707106781,0.577350269,-0.40824829);
+  disk_t yes_disk_2 = new_disk(1.2,center,normal);
+
+  vector_t disk_intersection_pt2 = new_vector(1.08298,0.276002,-0.463416);
   
-  //a disk out of the way
+  //positive test, observer 3
+  center = new_vector(-0.3,4.5,6.9);
+  normal = new_vector(0.6,0.3,0.741619849);
+  disk_t yes_disk_3 = new_disk(1.2,center,normal);
+
+  vector_t disk_intersection_pt3 = new_vector(-0.246197,4.7538,6.7538);
+  
+  //negative test, observer 1
   center = new_vector(0.9,-0.1,2.2);
   normal = new_vector(0.96,0.,-0.28);
-  disk no_disk_1 = new_disk(1.,center,normal);
+  disk_t no_disk_1 = new_disk(1.,center,normal);
 
-  //a disk behind him
+  //negative test, observer 2
+  center = new_vector(1.1,-0.3,-0.4);
+  normal = new_vector(0.707106781,0.577350269,-0.40824829);
+  disk_t no_disk_2 = new_disk(0.5,center,normal);
+  
+  //negative test, observer 3
+  center = new_vector(1.3,4.5,6.9);
+  normal = new_vector(0.6,0.3,0.741619849);
+  disk_t no_disk_3 = new_disk(0.4,center,normal);
+  
+  //edge case, disk behind observer 1
   center = new_vector(-0.7,1.1,1.);
   normal = new_vector(0.96,-0.28,0.);
-  disk no_disk_2 = new_disk(2.5,center,normal);
+  disk_t no_disk_4 = new_disk(2.5,center,normal);
 
-  //a disk parallel to him, but off to the side
+  //edge case, disk to the side of observer 1
   center = new_vector(2.5,-1.3,1.2);
   normal = new_vector(0.,0.,1.);
-  disk no_disk_3 = new_disk(2.,center,normal);
+  disk_t no_disk_5 = new_disk(2.,center,normal);
 
-  //a cylinder in his way
+  //positive test, observer 1
   center = new_vector(0.9,-0.7,0.4);
   axis = new_vector(0.8,0.,0.6); 
-  cylinder yes_cylinder = new_cylinder(0.5, 2.,center,axis);
+  cylinder_t yes_cylinder_1 = new_cylinder(0.5, 2.,center,axis);
   
-  vector cylinder_intersection_pt = new_vector(1.179421,-0.309566,1.);
+  vector_t cylinder_intersection_pt1 = new_vector(1.179421,-0.309566,1.);
 
-  //a cylinder not in his way
+  //positive test, observer 2
+  center = new_vector(3.3,0.7,-0.1);
+  axis = new_vector(-0.707106781,0.577350269,0.40824829);
+  cylinder_t yes_cylinder_2 = new_cylinder(1.,3.,center,axis);
+
+  vector_t cylinder_intersection_pt2 = new_vector(1.76334,0.831516,-0.0706083);
+  
+  //positive test, observer 3
+  center = new_vector(0.4,5.7,8.74);
+  axis = new_vector(0.6,0.3,-0.741619849);
+  cylinder_t yes_cylinder_3 = new_cylinder(1.,1.4,center,axis);
+
+  vector_t cylinder_intersection_pt3 = new_vector(0.419612,5.41961,7.41961);
+  
+  //negative test, observer 1
   center = new_vector(0.9,1.4,0.3);
   axis = new_vector(0.8,0.,0.6);
-  cylinder no_cylinder_1 = new_cylinder(0.5, 2., center, axis);
+  cylinder_t no_cylinder_1 = new_cylinder(0.5, 2., center, axis);
 
-  //a cylinder behind him
+  //negative test, observer 2
+  center = new_vector(1.3,-1.7,1.1);
+  axis = new_vector(-0.707106781,0.577350269,0.40824829);
+  cylinder_t no_cylinder_2 = new_cylinder(1.,3.,center,axis);
+
+  //negative test, observer 3
+  center = new_vector(0.4,3.7,8.74);
+  axis = new_vector(0.6,0.3,-0.741619849);
+  cylinder_t no_cylinder_3 = new_cylinder(1.,1.4,center,axis);
+  
+  //edge case: cylinder behind observer 1
   center = new_vector(-2.3,1.7,0.4);
   axis = new_vector(-2.3,1.7,0.4);
-  cylinder no_cylinder_2 = new_cylinder(0.5, 2., center, axis);
+  cylinder_t no_cylinder_4 = new_cylinder(0.5, 2., center, axis);
 
-  //a cylinder parallel to him
+  //edge case: observer 1 looking along the axis
   center = new_vector(0.9,-0.1,1.);
   axis = new_vector(0.8,-0.6,0.);
-  cylinder no_cylinder_3 = new_cylinder(0.5, 2., center, axis);
+  cylinder_t no_cylinder_5 = new_cylinder(0.5, 2., center, axis);
+
+  //edge case: observer 3 peering under the lower rim
+  center = new_vector(2.,7.,9.);
+  axis = new_vector(0.6,-0.3,0.741619849);
+  cylinder_t yes_cylinder_4 = new_cylinder(1.,2.4,center,axis);
+
+  vector_t cylinder_intersection_pt4 = new_vector(2.72262,7.72262,9.72262);
+
+  //edge case: observer 3 peering over the upper rim
+  center = new_vector(0.8,7.6,10.4832);
+  axis = new_vector(0.6,-0.3,-0.741619849);
+  cylinder_t yes_cylinder_5 = new_cylinder(1.,2.,center,axis);
+
+  vector_t cylinder_intersection_pt5 = new_vector(2.59707,7.59707,9.59707);
   
-  //a cone in his way
+  //positive test, observer 1
   vertex = new_vector(0.9,-0.7,0.4);
   axis = new_vector(0.8,0.,0.6);
-  cone yes_cone = new_cone(1., 2., vertex, axis);
+  cone_t yes_cone_1 = new_cone(1., 2., vertex, axis);
 
-  vector cone_intersection_pt = new_vector(1.33248,-0.42436,1.);
+  vector_t cone_intersection_pt1 = new_vector(1.33248,-0.42436,1.);
+
+  //positive test, observer 2
+  vertex = new_vector(1.9,1.8,-0.1);
+  axis = new_vector(0.707106781,-0.577350269,0.40824829);
+  cone_t yes_cone_2 = new_cone(2.,2.,vertex,axis);
+
+  vector_t cone_intersection_pt2 = new_vector(1.976,1.00515,0.0521684);
   
-  //a cone behind him
-  vertex = new_vector(-2.3,1.7,0.4);
-  normal = new_vector(0.8,0.,0.6);
-  cone no_cone_1 = new_cone(1., 2., vertex, axis);
+  //positive test, observer 3
+  vertex = new_vector(0.4,5.7,8.74);
+  axis = new_vector(0.6,0.3,-0.741619849);
+  cone_t yes_cone_3 = new_cone(2.,4.,vertex,axis);
 
-  //a cone not in his way
+  vector_t cone_intersection_pt3 = new_vector(0.722587,5.72259,7.72259);
+  
+  //negative test: observer 1
   vertex = new_vector(0.9,1.4,0.3);
   axis = new_vector(0.8,0.,0.6);
-  cone no_cone_2 = new_cone(0.5, 2., vertex, axis);
+  cone_t no_cone_1 = new_cone(0.5, 2., vertex, axis);
+  
+  //negative test, observer 2
+  vertex = new_vector(-0.9,2.8,3.1);
+  axis = new_vector(0.707106781,-0.577350269,0.40824829);
+  cone_t no_cone_2 = new_cone(1.,2.,vertex,axis);
 
-  //a cone barely not in his way (cylinder vs cone)
+  //negative test, observer 3
+  vertex = new_vector(1.4,4.3,3.3);
+  axis = new_vector(0.6,-0.3,0.741619849);
+  cone_t no_cone_3 = new_cone(1.,2.,vertex,axis);
+  
+  //edge case: a cone behind observer 1
+  vertex = new_vector(-2.3,1.7,0.4);
+  normal = new_vector(0.8,0.,0.6);
+  cone_t no_cone_4 = new_cone(1., 2., vertex, axis);
+
+  //edge case: a cone not in view of observer 1 whose circumscribed cylinder is in view
   vertex = new_vector(0.9,0.,0.4);
   axis = new_vector(0.8,0.,0.6);
-  cone no_cone_3 = new_cone(2.0, 4., vertex, axis);
+  cone_t no_cone_5 = new_cone(2.0, 4., vertex, axis);
 
-  vector intersection;
+  //edge case: observer 3 peering over the rim
+  vertex = new_vector(0.8,7.6,10.4832);
+  axis = new_vector(0.6,-0.3,-0.741619849);
+  cone_t yes_cone_4 = new_cone(1.,2.,vertex,axis);
+
+  vector_t cone_intersection_pt4 = new_vector(2.52752,7.52752,9.52752);
+  
+  vector_t intersection;
   double test_tol = 1e-4;
 
   printf("\n\n Sphere tests: \n\n");
@@ -116,63 +219,141 @@ int main(int argc, char ** argv) {
   
   printf("\n\n Disk tests: \n\n");
 
-  if (!ray_disk_intersection(observer,yes_disk,&intersection))
+  if (!ray_disk_intersection(observer,yes_disk_1,&intersection))
     printf("Test 4 failed: This disk should intersect the ray\n");
-  else if (distance(intersection,disk_intersection_pt) > test_tol)
-    printf("Test 4 failed: Intersection point off by %g\n",distance(intersection,disk_intersection_pt));
+  else if (distance(intersection,disk_intersection_pt1) > test_tol)
+    printf("Test 4 failed: Intersection point off by %g\n",distance(intersection,disk_intersection_pt1));
   else printf("Test 4 Passed\n");
 
-  if (ray_disk_intersection(observer,no_disk_1,&intersection))
-    printf("Test 5 failed: This disk should not intersect the ray\n");
+  if (!ray_disk_intersection(observer2,yes_disk_2,&intersection))
+    printf("Test 5 failed: This disk should intersect the ray\n");
+  else if (distance(intersection,disk_intersection_pt2) > test_tol)
+  printf("Test 5 failed: Intersection point off by %g\n",distance(intersection,disk_intersection_pt2));
   else printf("Test 5 Passed\n");
-
-  if (ray_disk_intersection(observer,no_disk_2,&intersection))
-    printf("Test 6 failed: This object should be directly behind the observer.\n");
+  
+  if (!ray_disk_intersection(observer3,yes_disk_3,&intersection))
+    printf("Test 6 failed: This disk should intersect the ray\n");
+  else if (distance(intersection,disk_intersection_pt3) > test_tol)
+  printf("Test 6 failed: Intersection point off by %g\n",distance(intersection,disk_intersection_pt3));
   else printf("Test 6 Passed\n");
 
-  if (ray_disk_intersection(observer,no_disk_3,&intersection))
-    printf("Test 7 failed: What happens when your disk is parallel?.\n");
+  if (ray_disk_intersection(observer,no_disk_1,&intersection))
+    printf("Test 7 failed: This disk should not intersect the ray\n");
   else printf("Test 7 Passed\n");
+
+  if (ray_disk_intersection(observer2,no_disk_2,&intersection))
+    printf("Test 8 failed: This disk should not intersect the ray\n");
+  else printf("Test 8 Passed\n");
+
+    if (ray_disk_intersection(observer3,no_disk_3,&intersection))
+    printf("Test 9 failed: This disk should not intersect the ray\n");
+  else printf("Test 9 Passed\n");
+  
+  if (ray_disk_intersection(observer,no_disk_4,&intersection))
+    printf("Test 10 failed: This object should be directly behind the observer.\n");
+  else printf("Test 10 Passed\n");
+
+  if (ray_disk_intersection(observer,no_disk_5,&intersection))
+    printf("Test 11 failed: What happens when your disk is parallel?.\n");
+  else printf("Test 11 Passed\n");
   
   printf("\n\n Cylinder tests: \n\n");
   
-  if (!ray_cylinder_intersection(observer,yes_cylinder,&intersection))
-    printf("Test 8 failed: This cylinder should intersect the ray\n");
-  else if (distance(intersection,cylinder_intersection_pt) > test_tol)
-    printf("Test 8 failed: Intersection point off by %g\n",distance(intersection,cylinder_intersection_pt));
-  else printf("Test 8 Passed\n");
-  
-  if (ray_cylinder_intersection(observer,no_cylinder_1,&intersection))
-    printf("Test 9 failed: This cylinder should not intersect the ray\n");
-  else printf("Test 9 Passed\n");
-
-  if (ray_cylinder_intersection(observer,no_cylinder_2,&intersection))
-    printf("Test 10 failed: This cylinder should be behind the observer.\n");
-  else printf("Test 10 Passed\n");
-
-  if (ray_cylinder_intersection(observer,no_cylinder_3,&intersection))
-    printf("Test 11 failed: Make sure your cylinder is open.\n");
-  else printf("Test 11 Passed\n");
-  
-  printf("\n\n Cone tests: \n\n");
-  
-  if (!ray_cone_intersection(observer,yes_cone,&intersection))
-    printf("Test 12 failed: This cone should intersect the ray\n");
-  else if (distance(intersection,cone_intersection_pt) > test_tol)
-    printf("Test 12 failed: Intersection point off by %g\n",distance(intersection,cone_intersection_pt));
+  if (!ray_cylinder_intersection(observer,yes_cylinder_1,&intersection))
+    printf("Test 12 failed: This cylinder should intersect the ray\n");
+  else if (distance(intersection,cylinder_intersection_pt1) > test_tol)
+    printf("Test 12 failed: Intersection point off by %g\n",distance(intersection,cylinder_intersection_pt1));
   else printf("Test 12 Passed\n");
-  
-  if (ray_cone_intersection(observer,no_cone_1,&intersection))
-    printf("Test 13 failed: This cone should not intersect the ray\n");
+
+  if (!ray_cylinder_intersection(observer2,yes_cylinder_2,&intersection))
+    printf("Test 13 failed: This cylinder should intersect the ray\n");
+  else if (distance(intersection,cylinder_intersection_pt2) > test_tol)
+    printf("Test 13 failed: Intersection point off by %g\n",distance(intersection,cylinder_intersection_pt2));
   else printf("Test 13 Passed\n");
 
-  if (ray_cone_intersection(observer,no_cone_2,&intersection))
-    printf("Test 14 failed: This cone should be behind the observer.\n");
+  if (!ray_cylinder_intersection(observer3,yes_cylinder_3,&intersection))
+    printf("Test 14 failed: This cylinder should intersect the ray\n");
+  else if (distance(intersection,cylinder_intersection_pt3) > test_tol)
+    printf("Test 14 failed: Intersection point off by %g\n",distance(intersection,cylinder_intersection_pt3));
   else printf("Test 14 Passed\n");
 
-  if (ray_cone_intersection(observer,no_cone_3,&intersection))
-    printf("Test 15 failed: Check to make sure your cone is not a cylinder.\n");
+  if (ray_cylinder_intersection(observer,no_cylinder_1,&intersection))
+    printf("Test 15 failed: This cylinder should not intersect the ray\n");
   else printf("Test 15 Passed\n");
+
+  if (ray_cylinder_intersection(observer2,no_cylinder_2,&intersection))
+    printf("Test 16 failed: This cylinder should not intersect the ray\n");
+  else printf("Test 16 Passed\n");
+
+    if (ray_cylinder_intersection(observer3,no_cylinder_3,&intersection))
+    printf("Test 17 failed: This cylinder should not intersect the ray\n");
+  else printf("Test 17 Passed\n");
+  
+  if (ray_cylinder_intersection(observer,no_cylinder_4,&intersection))
+    printf("Test 18 failed: This cylinder should be behind the observer.\n");
+  else printf("Test 18 Passed\n");
+
+  if (ray_cylinder_intersection(observer,no_cylinder_5,&intersection))
+    printf("Test 19 failed: Make sure your cylinder is open.\n");
+  else printf("Test 19 Passed\n");
+
+  if (!ray_cylinder_intersection(observer3,yes_cylinder_4,&intersection))
+    printf("Test 20 failed: This cylinder should intersect the ray.  Check your bounds.\n");
+  else if (distance(intersection,cylinder_intersection_pt4) > test_tol)
+    printf("Test 20 failed: Intersection point off by %g.  Check your bounds.\n",distance(intersection,cylinder_intersection_pt4));
+  else printf("Test 20 Passed\n");
+
+  if (!ray_cylinder_intersection(observer3,yes_cylinder_5,&intersection))
+    printf("Test 21 failed: This cylinder should intersect the ray.  Check your bounds\n");
+  else if (distance(intersection,cylinder_intersection_pt5) > test_tol)
+    printf("Test 21 failed: Intersection point off by %g.  Check your bounds.\n",distance(intersection,cylinder_intersection_pt5));
+  else printf("Test 21 Passed\n");
+    
+  printf("\n\n Cone tests: \n\n");
+  
+  if (!ray_cone_intersection(observer,yes_cone_1,&intersection))
+    printf("Test 22 failed: This cone should intersect the ray\n");
+  else if (distance(intersection,cone_intersection_pt1) > test_tol)
+    printf("Test 22 failed: Intersection point off by %g\n",distance(intersection,cone_intersection_pt1));
+  else printf("Test 22 Passed\n");
+
+  if (!ray_cone_intersection(observer2,yes_cone_2,&intersection))
+    printf("Test 23 failed: This cone should intersect the ray\n");
+  else if (distance(intersection,cone_intersection_pt2) > test_tol)
+    printf("Test 23 failed: Intersection point off by %g\n",distance(intersection,cone_intersection_pt2));
+  else printf("Test 23 Passed\n");
+
+  if (!ray_cone_intersection(observer3,yes_cone_3,&intersection))
+    printf("Test 24 failed: This cone should intersect the ray\n");
+  else if (distance(intersection,cone_intersection_pt3) > test_tol)
+    printf("Test 24 failed: Intersection point off by %g\n",distance(intersection,cone_intersection_pt3));
+  else printf("Test 24 Passed\n");
+  
+  if (ray_cone_intersection(observer,no_cone_1,&intersection))
+    printf("Test 25 failed: This cone should not intersect the ray\n");
+  else printf("Test 25 Passed\n");
+
+  if (ray_cone_intersection(observer2,no_cone_2,&intersection))
+    printf("Test 26 failed: This cone should not intersect the ray\n");
+  else printf("Test 26 Passed\n");
+
+    if (ray_cone_intersection(observer3,no_cone_3,&intersection))
+    printf("Test 27 failed: This cone should not intersect the ray\n");
+  else printf("Test 27 Passed\n");
+  
+  if (ray_cone_intersection(observer,no_cone_4,&intersection))
+    printf("Test 28 failed: This cone should be behind the observer.\n");
+  else printf("Test 28 Passed\n");
+
+  if (ray_cone_intersection(observer,no_cone_5,&intersection))
+    printf("Test 29 failed: Check to make sure your cone is not a cylinder.\n");
+  else printf("Test 29 Passed\n");
+
+  if (!ray_cone_intersection(observer3,yes_cone_4,&intersection))
+    printf("Test 30 failed: This cone should intersect the ray\n");
+  else if (distance(intersection,cone_intersection_pt3) > test_tol)
+    printf("Test 24 failed: Intersection point off by %g\n",distance(intersection,cone_intersection_pt3));
+  else printf("Test 30 Passed\n");
   
   return 0;
 }
